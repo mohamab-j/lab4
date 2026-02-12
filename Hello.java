096e2667d3cec3d65546cd77e0df3a9dc49e9bfc
Lab4 User <mwendwatk76@gmail.com>
2026-02-13 01:31:11 +0300

    Add Hello.java

--- Hello.java
+++ Hello.java
@@ -1,20 +1,7 @@
 public class Hello {
-	public static void main(String[] args) {
-
-		for(int i=1;i<=10;i++) {
-
-			System.out.println("Hello World..."+i);
-		}
-		
-		// New feature: Print squared numbers
-		printSquares();
-	}
-	
-	// New method to print square numbers
-	public static void printSquares() {
-		System.out.println("\nSquare numbers:");
-		for(int i=1; i<=5; i++) {
-			System.out.println(i + " squared = " + (i * i));
-		}
-	}
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++) {
+            System.out.println("Hello World..."+i);
+        }
+    }
 }
