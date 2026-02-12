6581e4ea761d394e1c30660dc3f51ef81fe2e7c4
Lab4 User <mwendwatk76@gmail.com>
2026-02-12 23:57:54 +0300

    Add square numbers feature to Hello.java

--- Hello.java
+++ Hello.java
@@ -5,5 +5,16 @@ public class Hello {
 
 			System.out.println("Hello World..."+i);
 		}
+		
+		// New feature: Print squared numbers
+		printSquares();
+	}
+	
+	// New method to print square numbers
+	public static void printSquares() {
+		System.out.println("\nSquare numbers:");
+		for(int i=1; i<=5; i++) {
+			System.out.println(i + " squared = " + (i * i));
+		}
 	}
 }
